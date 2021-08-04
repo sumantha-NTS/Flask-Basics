1d4dd1d0a20734ae483bf956733b60bfde0443b2
Sumantha N.T.S <sumanthneechadi@gmail.com>
2021-08-04 19:31:21 +0530

    email application error

--- 04-Aug-2021/Email/email.py
+++ 04-Aug-2021/Email/email.py
@@ -1,6 +1,23 @@
 from flask import Flask
-from flask_mail import Mail
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
+# configuration of mail
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'sumanthnts.96@gmail.com'
+app.config['MAIL_PASSWORD'] = '******'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
+
+@app.route("/")
+def index():
+   msg = Message('Hello',sender ='sumanthnts.96@gmail.com',recipients = ['sumanthneechadi@gmail.com'])
+   msg.body = 'Hello Flask message sent from Flask-Mail'
+   mail.send(msg)
+   return 'Sent'
+   
+if __name__ == '__main__':
+   app.run(debug = True)
