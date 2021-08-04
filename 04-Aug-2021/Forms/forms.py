52d257266db72ca338ca9b9b42b8ea5696fc37c8
Sumantha N.T.S <sumanthneechadi@gmail.com>
2021-08-04 14:28:50 +0530

    Form application

--- 04-Aug-2021/Forms/forms.py
+++ 04-Aug-2021/Forms/forms.py
@@ -1,5 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = "hard to guess string"
+
+class Nameform(FlaskForm):
+    name = StringField('What is your name..?',validators=[DataRequired()])
+    location = StringField('Your Location')
+    submit = SubmitField('Submit')
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    name = None
+    location = None
+    form = Nameform()
+    if form.validate_on_submit():
+        name = form.name.data
+        location = form.location.data
+        form.name.data = ''
+    return render_template('index.html',form = form, name=name)
