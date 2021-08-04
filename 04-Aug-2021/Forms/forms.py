44bb3db0fefc9405c95429d50e3482acbf40360a
Sumantha N.T.S <sumanthneechadi@gmail.com>
2021-08-04 16:01:41 +0530

    forms page completed

--- 04-Aug-2021/Forms/forms.py
+++ 04-Aug-2021/Forms/forms.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session, url_for
+from flask.helpers import flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -9,18 +10,22 @@ bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = "hard to guess string"
 
+### creating class for form
 class Nameform(FlaskForm):
     name = StringField('What is your name..?',validators=[DataRequired()])
-    location = StringField('Your Location')
     submit = SubmitField('Submit')
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    name = None
-    location = None
-    form = Nameform()
+    form = Nameform()      ## calling the form class and initiating to form varible 
     if form.validate_on_submit():
-        name = form.name.data
-        location = form.location.data
-        form.name.data = ''
-    return render_template('index.html',form = form, name=name)
+        old_name = session.get('name')
+        if (old_name is not None) and (old_name!=form.name.data):       ## just to flash a message
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data        ### getting the name data
+        flash('Nice name!!!')
+        return redirect(url_for('index'))       ### redirecting to index function again
+    return render_template('index.html',form = form, name=session.get('name')) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
