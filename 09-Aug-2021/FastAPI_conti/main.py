4d375d5f030efa79321f70924d62442c6f7b2af1
Sumantha NTS <sumanthneechadi@gmail.com>
2021-08-09 11:45:27 +0530

    added delete and update route

--- 09-Aug-2021/FastAPI_conti/main.py
+++ 09-Aug-2021/FastAPI_conti/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends, status, Response
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.expression import false
 import schemas,models
 from database import SessionLocal, engine
 
@@ -16,6 +17,9 @@ def get_db():
     finally:
         db.close()
 
+@app.get('/')
+def welocme():
+    return {'message':'App is working'}
 
 #post decorator
 @app.post('/blog',status_code=status.HTTP_201_CREATED)
@@ -26,8 +30,21 @@ def create_blog(blog:schemas.blog, db:Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id: int, db:Session = Depends(get_db)):
+    db.query(models.blog).filter(models.blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return {'message':f'BBlog with id deleted'}
+
+@app.put('/blog/{id}',status_code = status.HTTP_202_ACCEPTED)
+def update(id: int, blog: schemas.blog, db:Session = Depends(get_db)):
+    db.query(models.blog).filter(models.blog.id == id).update({'title':blog.title,'author' : blog.author},synchronize_session=False)
+    db.commit()
+    updated = db.query(models.blog).filter(models.blog.id == id).first()
+    return {'message':{'updated data':updated}}
+
 @app.get('/blog')
-def show_all(db:Session = Depends(get_db),limit: int = 2):
+def show_all(db:Session = Depends(get_db),limit: int = 10):
     blogs = db.query(models.blog).limit(limit).all()
     return blogs
 
@@ -37,7 +54,6 @@ def show_blog(id : int, response: Response, db:Session = Depends(get_db)):
     if not blog_1:
         response.status_code = status.HTTP_404_NOT_FOUND
         return {'message':'Blog id is out of range'}
-
     return blog_1
 
 
